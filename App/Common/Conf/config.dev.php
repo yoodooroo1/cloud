94b6dbe5457d8ff1f6d8290cb71b918de94d7162
ydr <541911483@qq.com>
2020-02-24 22:13:06 +0800

    feat: 代码提交(未完成)

--- App/Common/Conf/config.dev.php
+++ App/Common/Conf/config.dev.php
@@ -15,7 +15,7 @@ return array(
     'DB_USER' => 'qcloud',      // 用户名
     'DB_PWD' => 'ebdsAN8fB3eMZfxG',          // 密码
     'DB_PORT' => '3306',        // 端口
-    'DB_PREFIX' => 'v_',    // 数据库表前缀
+    'DB_PREFIX' => 'lm_',    // 数据库表前缀
     'DB_PARAMS' => array(), // 数据库连接参数
     'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE' => true,        // 启用字段缓存
