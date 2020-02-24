0b8ba54fcfc5ab069dd5aee1e176f5978e81e1b1
ydr <541911483@qq.com>
2020-02-24 22:22:29 +0800

    feat: 代码提交(未完成)

--- App/Common/Conf/config.common.php
+++ App/Common/Conf/config.common.php
@@ -8,13 +8,14 @@
 defined('YYS_URL') or define('YYS_URL', "http://jiameng.duinin.com/");
 
 return array(
+    /* 数据库设置 */
     'DB_TYPE' => 'mysql',     // 数据库类型
-    'DB_HOST' => '127.0.0.1', // 服务器地址
-    'DB_NAME' => 'qcloud',          // 数据库名
-    'DB_USER' => 'qcloud',      // 用户名
-    'DB_PWD' => 'ebdsAN8fB3eMZfxG',          // 密码
+    'DB_HOST' => '118.89.53.225', // 服务器地址
+    'DB_NAME' => 'qqcg_jm_db',          // 数据库名
+    'DB_USER' => 'duininjiameng',      // 用户名
+    'DB_PWD' => 'mysUL5bX',          // 密码
     'DB_PORT' => '3306',        // 端口
-    'DB_PREFIX' => 'v_',    // 数据库表前缀
+    'DB_PREFIX' => 'lm_',    // 数据库表前缀
     'DB_PARAMS' => array(), // 数据库连接参数
     'DB_DEBUG' => TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE' => true,        // 启用字段缓存
